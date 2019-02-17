d6df536f2931b8a66924a2f0a1ac8c925c81073c
qiuq <qaq1937@163.com>
2019-02-17 18:07:12 +0800

    sssss

---
+++ git-test-projest/git-test-projest01/src/com/itcast/FistClass.java
@@ -0,0 +1,10 @@
+package com.itcast;
+
+public class FistClass {
+    public static void main(String[] args) {
+        System.out.println("jldhalhf");
+        System.out.println("jldhalhf");
+        System.out.println("jldhalhf");
+        System.out.println("jldhalhf");
+    }
+}
